1d50adf15a645a25763b1b3b53a3573d63572bc2
mihir-pandhe <pandhemihir@gmail.com>
2024-04-12 18:59:45 +0530

    Only Readme Left

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm<span className="text-[#915EFF]"> Harsh</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I developed this
+            I have developed this
             <br className="sm:block hidden" />
             Collaborative Code Editor{" "}
           </p>
